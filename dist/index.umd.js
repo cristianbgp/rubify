260af49b898355b7a1ca48ff4bb88629651ba25b
cristianbgp <cristian.granda.pastor@gmail.com>
2019-06-04 17:46:20 -0500

    Delete dist directory

--- dist/index.umd.js
+++
@@ -1,2 +0,0 @@
-!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?module.exports=o():"function"==typeof define&&define.amd?define(o):e.rubify=o()}(this,function(){function e(){for(var e=[],o=arguments.length;o--;)e[o]=arguments[o];console.log.apply(console,e)}return e("asd"),e(["a","asd","asdq"],"asdasd",1,!0,{a:1,asd:123}),e});
-//# sourceMappingURL=index.umd.js.map
