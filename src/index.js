4c725696611d881a0c42f327d7a9bb41166744a7
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-06-04 22:48:05 -0500

    Add some more methods

--- src/index.js
+++ src/index.js
@@ -8,3 +8,37 @@ const reverse = string => {
     }, "");
   }
 };
+
+const empty = something => {
+  return something.length === 0;
+};
+
+const chars = string => {
+  return string.split("");
+};
+
+const downcase = string => {
+  return string.toLowerCase();
+};
+
+const upcase = string => {
+  return string.toUpperCase();
+};
+
+const even = number => {
+  if (typeof number === "number") {
+    return number % 2 === 0;
+  }
+};
+
+const odd = number => {
+  if (typeof number === "number") {
+    return number % 2 != 0;
+  }
+};
+
+const times = (number, callback) => {
+  for (let index = 1; index <= number; index++) {
+    callback(index);
+  }
+};
